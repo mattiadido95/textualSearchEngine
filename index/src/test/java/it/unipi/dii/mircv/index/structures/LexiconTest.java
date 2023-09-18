c993a1b13deb038ca061776d5176258645bf3677
mattia.didonato95 <mattia.didonato95@gmail.com>
2023-09-18 12:42:30 +0200

    fix random

--- index/src/test/java/it/unipi/dii/mircv/index/structures/LexiconTest.java
+++ index/src/test/java/it/unipi/dii/mircv/index/structures/LexiconTest.java
@@ -8,21 +8,29 @@ import org.junit.jupiter.api.TestInstance;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Random;
+
 import java.util.TreeMap;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class LexiconTest {
-    TreeMap<String, LexiconElem> lexicon;
+    private TreeMap<String, LexiconElem> lexicon;
 
     @BeforeAll
     public void setUp() {
         // Inizializza l'oggetto BlockDescriptor qui, se necessario
-        this.lexicon = new TreeMap<String, LexiconElem>();
+        this.lexicon = new TreeMap<>();
     }
 
+    @Test
+    public void testLexicon() {
+//        new LexiconTest();
+        addLexiconElem("cane");
+        addLexiconElem("gatto");
+        addLexiconElem("topo");
+    }
 
     public LexiconTest() {
-
     }
 
     private void addLexiconElem(String term) {
@@ -34,20 +42,17 @@ public class LexiconTest {
 
     private PostingList createPostinList() {
         PostingList pl = new PostingList();
-        int num = (int) (Math.random() * 10);
+        Random random = new Random();
+        // Genera un numero casuale tra 0 (incluso) e 11 (escluso)
+        int num = random.nextInt(6);
+        int previous = 0;
         for (int i = 0; i < 50 - num; i++) {
-            Posting p = new Posting(i, i % 6);
+            Posting p = new Posting(previous + random.nextInt(8) + 1, i % 6);
             pl.getPostings().add(p);
+            previous = p.getDocID();
         }
         return pl;
     }
 
-    @Test
-    public void testLexicon() {
-        new LexiconTest();
-        addLexiconElem("cane");
-        addLexiconElem("gatto");
-        addLexiconElem("topo");
-    }
 
 }
