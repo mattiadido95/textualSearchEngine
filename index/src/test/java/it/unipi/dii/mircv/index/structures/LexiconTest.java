c2fcd3e74c96cd7ff92198e266cca31d2cf4b7a5
mgiorgi13 <96mat96@libero.it>
2023-09-18 12:41:42 +0200

    fix setUp

--- index/src/test/java/it/unipi/dii/mircv/index/structures/LexiconTest.java
+++ index/src/test/java/it/unipi/dii/mircv/index/structures/LexiconTest.java
@@ -1,38 +1,28 @@
 package it.unipi.dii.mircv.index.structures;
 
-import org.junit.Test;
-import org.junit.jupiter.api.TestInstance;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.TestInstance;
-
+import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-import java.util.TreeMap;
-
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class LexiconTest {
-    TreeMap<String, LexiconElem> lexicon;
+    Lexicon lexicon = new Lexicon();
 
-    @BeforeAll
+    @BeforeEach
     public void setUp() {
         // Inizializza l'oggetto BlockDescriptor qui, se necessario
-        this.lexicon = new TreeMap<String, LexiconElem>();
-    }
-
-
-    public LexiconTest() {
-
+        lexicon.addLexiconElem("cane");
+        lexicon.addLexiconElem("gatto");
+        lexicon.addLexiconElem("topo");
     }
 
     private void addLexiconElem(String term) {
-        PostingList pl = createPostinList();
+        PostingList pl = createPostingList();
         long offset = pl.savePostingListToDisk(-2);
         LexiconElem elem = new LexiconElem(pl.getPostingListSize(), 0, offset, 0, 0, 0);
-        this.lexicon.put(term, elem);
+        lexicon.getLexicon().put(term, elem);
     }
 
-    private PostingList createPostinList() {
+    private PostingList createPostingList() {
         PostingList pl = new PostingList();
         int num = (int) (Math.random() * 10);
         for (int i = 0; i < 50 - num; i++) {
@@ -43,11 +33,12 @@ public class LexiconTest {
     }
 
     @Test
-    public void testLexicon() {
-        new LexiconTest();
-        addLexiconElem("cane");
-        addLexiconElem("gatto");
-        addLexiconElem("topo");
+    public void testAddLexiconElem() {
+        // Test per verificare che l'aggiunta di elementi al lexicon funzioni correttamente
+        // Assicurati di implementare questa logica nel tuo Lexicon
+        // Verifica che gli elementi siano stati aggiunti correttamente al lexicon
+        assertTrue(lexicon.getLexicon().containsKey("cane"));
+        assertTrue(lexicon.getLexicon().containsKey("gatto"));
+        assertTrue(lexicon.getLexicon().containsKey("topo"));
     }
-
 }
